f02904d341d8594bed7049a2013b7afe25e790f9
Cosmo19 <38812494+Cosmo19@users.noreply.github.com>
2018-04-28 14:56:47 +0100

    Add files via upload

---
+++ LogicTask1/LogicTask1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTask1
+{
+    class Program
+    {
+        static List<int> numberList = new List<int>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example Logic Task 1");
+
+            string a;
+
+            while (true)
+            {
+                Console.Write("Amount: ");
+
+                a = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(a) && IsInteger(a))
+                    break;
+            }
+
+            for (int i = 1; Convert.ToInt32(a) >= i; i++)
+            {
+                string b;
+                
+                while (true)
+                {
+                    Console.Write("Number " + i + ": ");
+
+                    b = Console.ReadLine();
+
+                    if (!String.IsNullOrEmpty(b) && IsInteger(b))
+                        break;
+                }
+
+                numberList.Add(Convert.ToInt32(b));
+            }
+
+            Console.WriteLine("Complete!");
+
+            Console.WriteLine("Greatest: " + numberList.Max());
+
+            Console.WriteLine("Lowest: " + numberList.Min());
+
+            Console.WriteLine("Average: " + numberList.Average());
+
+            Console.WriteLine("Total: " + numberList.Sum());
+
+            Console.WriteLine("Press any key to terminate.");
+
+            numberList.Clear();
+
+            Console.ReadKey();
+        }
+
+        static bool IsInteger(string s)
+        {
+            try
+            {
+                Int32.TryParse(s, out int r);
+
+                if (r == 0)
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
